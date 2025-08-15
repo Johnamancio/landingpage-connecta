c22538629032e53f5886ba8acb7c77f15d85d3a9
Johnatatwd@gmail.com <johnatatwd@gmail.com>
2025-08-14 23:08:41 -0300

    Tratamento de classes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <header>
-    <nav>
+    <nav class="navbar">
         <div class="logo">
             <a href="/">
             <img src="{{ asset('images/logo.png') }}" alt="Logo da Empresa">
              </a>
         </div>
-        
+       <div class="nomeMenu">
         <ul id="menu">
             <li><a href="#inicio">Início</a></li>
             <li><a href="#sobre">Sobre</a></li>
@@ -14,6 +14,7 @@
             <li><a href="#depoimentos">Depoimentos</a></li>
             <li><a href="#contato">Contato</a></li>
         </ul>
+        </div>
         <!-- Botão do menu -->
         <div id="menu-toggle" class="menu-toggle">&#9776;</div>
     </nav>
